4442dc018218e2727661fbb1a60a7de1425bee00
Destroy-Youth <luis.herrerae@outlook.com>
2017-12-19 13:21:57 -0600

    Update MongounitecApplication.java

--- src/main/java/unitec/org/mongounitec/MongounitecApplication.java
+++ src/main/java/unitec/org/mongounitec/MongounitecApplication.java
@@ -28,8 +28,10 @@ public class MongounitecApplication implements CommandLineRunner{
 
     @Override
     public void run(String... strings) throws Exception {
+	    /*
         Mensajito mensa = new Mensajito("Primero mongo", "mi primer mongo");
         repoMensa.save(mensa);
             System.out.println("Mensaje guardado");
+	    */
     }
 }
